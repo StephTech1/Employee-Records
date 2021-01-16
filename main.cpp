f82f95f55f203e90d89cc76a61c633ace8dcb849
Steph St <steph.streete@outlook.com>
2021-01-16 20:43:51 +0000

    added code for first  employee

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+using namespace std;
+
+class employee{
+  public:
+  string name;
+  int year;
+  string salary;
+  string address;
+  };
 
 int main() {
-  std::cout << "Hello World!\n";
+  
+employee e1;
+e1.name = "Robert";
+e1.year = 2018;
+e1.salary = "£25000";
+e1.address = "64c London Street";
+
+cout <<e1.name<< " "<<e1.year<<" "<<e1.salary<< " "<<e1.address<< endl;
 }
